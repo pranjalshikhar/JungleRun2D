b403618ea18f19de3708b753cb10c8d81d1d57c7
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-01-11 16:03:45 +0530

    Player Designed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidBody;
+   
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+    }
+}
